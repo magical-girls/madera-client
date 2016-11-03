2c6629841c2bd85613d7d39729964c5d56f3d890
JVanhouteghem <vanhouteghem.j@gmail.com>
2016-11-03 14:25:29 +0100

    Update listeDevis : create function to change state color

--- js/controllers.js
+++ js/controllers.js
@@ -4,60 +4,80 @@
 
 angular
   .module('Madera', ['ui.bootstrap', 'ngSanitize', 'angular.filter'])
-/////////////////////////////// Sign in controller  ///////////////////////////////////////////////////
-.controller('connectCtrl', ['$scope', function($scope) {
-  var user ="user";
-  var pass = "pass";
-  console.log("coucou");
-  $scope.checkUser= function () {
+  /////////////////////////////// Sign in controller  ///////////////////////////////////////////////////
+  .controller('connectCtrl', ['$scope', function ($scope) {
+    var user = "user";
+    var pass = "pass";
+    console.log("coucou");
+    $scope.checkUser = function () {
       // Récupération de variables, voir : http://www.w3schools.com/angular/tryit.asp?filename=try_ng_form
-    console.log("name : " + $scope.login);
-    console.log("pass : " + $scope.pass);
-    //var urlHor = "http://app-f84c6d3d-ce9d-4499-a234-4cfcdd148e5e.cleverapps.io/user/auth";
-    console.log("user récup = "+user);
-    if ($scope.login == user) {
-      if ($scope.pass == pass) {
-        console.log("connexion ok");
+      console.log("name : " + $scope.login);
+      console.log("pass : " + $scope.pass);
+      //var urlHor = "http://app-f84c6d3d-ce9d-4499-a234-4cfcdd148e5e.cleverapps.io/user/auth";
+      console.log("user récup = " + user);
+      if ($scope.login == user) {
+        if ($scope.pass == pass) {
+          console.log("connexion ok");
+        }
+      } else {
+        console.log("Aucun utilisateur de ce nom la");
       }
-    }else {
-      console.log("Aucun utilisateur de ce nom la");
-    }
-  };
-}])
-
-/////////////////////// Header controler (navbar) ////////////////////////////////////////////////
-.controller('HeaderController', ['$scope', function($scope) {
-  function HeaderController($scope, $location){
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
     };
-  }
   }])
-////////////////////////  list controler  ///////////////////////////////////////////////////////
-.controller('devisCtrl', function($scope, $http) {
-  $scope.sortType     = 'num_devis'; // tri sur le num_devis par defaut
-  $scope.sortReverse  = false;  // sens du tri par defaut
-  $scope.searchText   = ''; // entrée saisie pour filtre
-  $http.get("listeDevis.json").then(function(response) {
-    $scope.datas = response.data;
-  });
 
-  $scope.confirmDelete= function () {
-    var message = "Etes-vous certain de vouloir supprimer ce devis?";
-    if(message && !confirm(message)){
-      console.log("pas supprimer");
-    }else {
-      console.log(" supprimer");
+  /////////////////////// Header controler (navbar) ////////////////////////////////////////////////
+  .controller('HeaderController', ['$scope', function ($scope) {
+    function HeaderController($scope, $location) {
+      $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+      };
     }
-  };
-})
+  }])
+  ////////////////////////  list controler  ///////////////////////////////////////////////////////
+  .controller('devisCtrl', function ($scope, $http) {
+    $scope.sortType = 'num_devis'; // tri sur le num_devis par defaut
+    $scope.sortReverse = false;  // sens du tri par defaut
+    $scope.searchText = ''; // entrée saisie pour filtre
+    $http.get("listeDevis.json").then(function (response) {
+      $scope.datas = response.data;
+    });
 
-// Formulaire client
-.controller('formCtrl', function($scope, $http) {
-  $http.get("client.json").then(function(response) {
-    $scope.datas = response.data;
-  });
-  console.log("test");
+    $scope.confirmDelete = function () {
+      var message = "Etes-vous certain de vouloir supprimer ce devis?";
+      if (message && !confirm(message)) {
+        console.log("pas supprimer");
+      } else {
+        console.log(" supprimer");
+      }
+    };
+
+    $scope.getAvancementColor = function (input) {
+      /*if (input == "en attente") {
+        return "orangeFont";
+      } 
+      else*/ if (input == "refusé") {
+        return "bold redFont";
+      }
+      /*else if (input == "validé") {
+        return "greyFont";
+      }  
+      else if (input == "en cours") {
+        return "blueFont";
+      }*/ 
+      else if (input == "terminé") {
+        return "bold greenFont";
+      }
+
+    }
+
+  })
+
+  // Formulaire client
+  .controller('formCtrl', function ($scope, $http) {
+    $http.get("client.json").then(function (response) {
+      $scope.datas = response.data;
+    });
+    console.log("test");
 
     $scope.isEmpty = function (input) {
       console.log(input[0]);
@@ -69,32 +89,31 @@ angular
       //return true; //$scope.data && $scope.data.client && $scope.data.client.adhesion.dateDepart && $scope.data.client.adhesion.dateDepart.substring(6) == $scope.currentYear;
     }
 
+    // Retourne un glyphicon avec une couleur verte si les champs sont bien remplis
     $scope.getIconValidator = function (input) {
       var result = true;
-      for (var i = 0 ; i < input.length ; i++){
-        if (input[i] == undefined || input[i] == ""){
+      for (var i = 0; i < input.length; i++) {
+        if (input[i] == undefined || input[i] == "") {
           result = false;
         }
       }
-      if (result == false){
-         return "redFont glyphicon glyphicon-remove";
-       } else {
+      if (result == false) {
+        return "redFont glyphicon glyphicon-remove";
+      } else {
         return "greenFont glyphicon glyphicon-ok";
-       }
-
-    }
-
-    $scope.oneAtATime = false;
-})
-.directive("test", function() {
-  return {
-    // A = attribut, C= classe, E = element
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      if(attrs.class == "reussi"){
-        element.addClass('text-success');
-        //$compile(element)(scope);
       }
     }
-  }
+    $scope.oneAtATime = false;
+  })
+  .directive("test", function () {
+    return {
+      // A = attribut, C= classe, E = element
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        if (attrs.class == "reussi") {
+          element.addClass('text-success');
+          //$compile(element)(scope);
+        }
+      }
+    }
   });
