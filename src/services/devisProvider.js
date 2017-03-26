94d9e88fdd3cc37e7af0df96066573f681f090cd
Melitchi <melissa.fontaine4@gmail.com>
2017-03-26 20:34:09 +0200

    begin http post

--- src/services/devisProvider.js
+++ src/services/devisProvider.js
@@ -1,77 +1,133 @@
 'use strict';
 // pour filtre dans select
-var devisLabel=[
-  {"name":"N° devis",
-    "value":"num_devis"},
-  {"name":"Client",
-    "value":"nom_client"},
-  {"name":"Création",
-    "value":"date_creation"},
-  {"name":"Dernière modification",
-    "value":"date_modif"},
-  {"name":"Etat",
-    "value":"etat"}
+var devisLabel = [
+  {
+    "name": "N° devis",
+    "value": "num_devis"
+  },
+  {
+    "name": "Client",
+    "value": "nom_client"
+  },
+  {
+    "name": "Création",
+    "value": "date_creation"
+  },
+  {
+    "name": "Dernière modification",
+    "value": "date_modif"
+  },
+  {
+    "name": "Etat",
+    "value": "etat"
+  }
 ];
 
-var aDevis=  {
-  "id_devis":"1",
-  "num_devis":"32",
-  "nom_commercial":"Homer Simpson",
-  "nom_client":"Squall Leonheart",
-  "date_creation":"01/01/2016",
-  "date_modif":"02/01/2016",
-  "etat":"en cours"
-};
-
-function getIndexDevisFromId(inputDevis, inputIdDevis){
+function getIndexDevisFromId(inputDevis, inputIdDevis) {
   var result = null;
-  for (var i = 0 ; i < inputDevis.length ; i++){
-    if (inputIdDevis == (inputDevis[i].num_devis)){
+  for (var i = 0; i < inputDevis.length; i++) {
+    if (inputIdDevis == (inputDevis[i].num_devis)) {
       result = i;
     }
   }
   return result;
 }
 
-app.service('devisProvider', function($http, $window, $routeParams){
-  this.getDevis = function(){
-	//url get devis
-	  return  {
-		    async: function() {
-		    	return $http({
-				  method : 'GET',
-				  url : host + 'devis',
-			  	  headers : {
-			  		'token' : $window.sessionStorage.getItem('token'),
-			  		'Content-type' : 'application/json; charset=UTF-8'
-			  	   }
-				  });
-				 } 
-		 }
+app.service('devisProvider', function ($http, $window, $routeParams) {
+  this.getDevis = function () {
+    //url get devis
+    return {
+      async: function () {
+        return $http({
+          method: 'GET',
+          url: host + 'devis',
+          headers: {
+            'token': $window.sessionStorage.getItem('token'),
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+        });
+      }
+    }
   }
-  this.deleteDevis = function(inputDevis, inputNumDevis){
+  this.deleteDevis = function (inputDevis, inputNumDevis) {
     inputDevis.splice(getIndexDevisFromId(inputDevis, inputNumDevis), 1);
   }
 
-  this.getaDevis = function(id){
-	//url get devis
-	  return  {
-		    async: function() {
-		    	return $http({
-				  method : 'GET',
-				  url : host + 'devis',
-				  params : {
-					  'id' : id
-				  },
-			  	  headers : {
-			  		'token' : $window.sessionStorage.getItem('token'),
-			  		'Content-type' : 'application/json; charset=UTF-8'
-			  	   }
-				  });
-				 } 
-		 }
+  this.getaDevis = function (id) {
+    //url get devis
+    return {
+      async: function () {
+        return $http({
+          method: 'GET',
+          url: host + 'devis',
+          params: {
+            'id': id
+          },
+          headers: {
+            'token': $window.sessionStorage.getItem('token'),
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+        });
+      }
+    }
   }
-  this.getDevisLabels = function(){
+  this.getDevisLabels = function () {
     return devisLabel;
+  }
+
+  this.saveDevis = function (nomClient, prenomClient, naissanceClient, telClient, adresseClient, professionClient, mailClient, idMatriculeSalarie,
+    referenceDevis, motifDevis, margeComDevis, margeEntDevis, idReferenceGamme,idRefModule,commentaireModule,sectionLongeur,refModule, angleType, angleDegre,moduleA,moduleB) {
+    //url get devis
+    return {
+      async: function () {
+        return $http({
+          url: host + 'devis',
+          method: "POST",
+          data: {
+            'nomClient': nomClient,
+            'prenomClient': prenomClient,
+            'naissanceClient': naissanceClient,
+            'telClient': telClient,
+            'adresseClient': adresseClient,
+            'professionClient': professionClient,
+            'mailClient': mailClient,
+            'idMatriculeSalarie': idMatriculeSalarie,
+            'referenceDevis': referenceDevis,
+            'motifDevis': motifDevis,
+            'margeComDevis': margeComDevis,
+            'margeEntDevis': margeEntDevis,
+            'idReferenceGamme': idReferenceGamme,
+            'lstModule':
+            [
+              {
+                'idReference': idRefModule,
+                'commentaire': commentaireModule
+              }
+            ],
+            'lstSection':
+            [
+              {
+                'longueur': sectionLongeur,
+                'refModule': refModule
+              }
+            ],
+
+            'lstAngle':
+            [
+              {
+                'type': angleType,
+                'degre': angleDegre,
+                'moduleA': moduleA,
+                'moduleB': moduleB
+              }
+            ]
+          },
+          headers: {
+            'token': $window.sessionStorage.getItem('token'),
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+        });
+      }
+    }
   }
 })
