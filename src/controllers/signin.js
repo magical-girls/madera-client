2f673d19781d11c8268a239e836e747a1055884f
emelfon <melissa.fontaine@ericsson.com>
2017-06-23 17:14:07 +0200

    range bug fixed for catalogue list
    await token solution when signin in progress

--- src/controllers/signin.js
+++ src/controllers/signin.js
@@ -2,9 +2,39 @@
 app.controller('signinCtrl', function ($scope, $rootScope, $location, $window, authentification, commonCode) {
   $scope.setToken = function () {
     if (!angular.isUndefined($scope.login) || !angular.isUndefined($scope.pass)) {
-      var check = authentification.setToken($scope.login, $scope.pass);
+    //  console.log( await authentification.setToken($scope.login, $scope.pass));
+    var check = authentification.setToken($scope.login, $scope.pass);
       // $scope.readToken = authentification.readToken();
-      console.log("check =" + authentification.setToken($scope.login, $scope.pass));
+      console.log("check = "+check);
+      if (check) {
+        $location.url("/accueil");
+        console.log("connection succeed");
+      } else {
+        // normalement failed et rester sur sign in
+        $location.url("/accueil");
+        console.log("connection failed");
+      }
+    } else {
+      commonCode.showAlertFieldsEmpty();
+    }
+  };
+})
+/*
+var encodedpass= window.btoa(unescape(encodeURIComponent($scope.pass)));
+      console.log("encoded = " +encodedpass);
+      console.log("decoded = " + window.atob(unescape(encodeURIComponent(encodedpass))));
+      */
+
+/////////////////////// Sign in controller  /////////////////////////////////////////////////
+/*app.controller('signinCtrl', function ($timeout, $scope, $location, $window, authentification, commonCode) {
+  $scope.setToken = function () {
+    if (!angular.isUndefined($scope.login) || !angular.isUndefined($scope.pass)) {
+      var check;
+      check = authentification.setToken($scope.login, $scope.pass);
+      
+      // $scope.readToken = authentification.readToken();
+      $timeout($scope.setToken, 5000);
+      console.log("check =" + check);
       if (check) {
         $location.url("/accueil");
         console.log("connection succeed");
--- src/services/authentification.js
+++ src/services/authentification.js
@@ -3,12 +3,12 @@ const host = 'http://78.198.68.4:12345/maderaserveur/';
 
 app.factory('authentification', function($window, $http){
 
-    return {
+   return {
         readToken: function () {
             return $window.sessionStorage.getItem('token');
         },
-        setToken: function(login, pass) {
-        	
+        setToken:  function(login, pass) {
+		var check;
             $http({
             	method : 'GET',
             	url : host + 'login',
@@ -19,24 +19,25 @@ app.factory('authentification', function($window, $http){
             	headers: { 
             		'Content-type' : 'application/json; charset=UTF-8'
 	        		}
-    	       })
-    	    .then(function(response) {
+    	       }).then(function(response) {
     	    	$window.sessionStorage.setItem('token', response.headers()["token"]);
     	    	$window.sessionStorage.setItem('login', response.data.login.login);
     	    	$window.sessionStorage.setItem('matricule', response.data.login.matriculeSalarie);
     	    	$window.sessionStorage.setItem('nom', response.data.salarie.nom);
     	    	$window.sessionStorage.setItem('prenom', response.data.salarie.prenom);
-    	    	return true;
+            check = true;
+            console.log("check1 =" + check);
     	    }, function(error) {
     	    	if(error.status === 401){
     	    		alert('Erreur d\'authentification')
     	    	} else {
     				alert('Erreur serveur');
     			}
-    	    	return false;
+            check = false;
+            console.log("check2 =" + check);
     	    });
-            
-        },
+			return check;
+		    },
         deleteToken: function() {
             $window.sessionStorage.clear();
             },
